1b2fb8dc134a75346330d22d525474bdf438f171
ysfu <a.yusuf.yildiz@hotmail.com>
2019-07-27 13:44:03 +0300

    Added some animation

--- scripts.js
+++ scripts.js
@@ -3,3 +3,12 @@ const $$ = selector => document.querySelectorAll(selector);
 
 const date = new Date();
 $('#age').innerText = (+(date.getFullYear()) - 1996)
+
+window.addEventListener('DOMContentLoaded', () => {
+	$("body").classList.add('loaded');
+}, false);
+
+var rellax = new Rellax('.rellax', {
+	round: true,
+	vertical: true,
+});
